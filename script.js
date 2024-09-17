b3faf645c1d25712970fc0cb6b72c0f63e4506b6
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-17 11:40:59 -0700

    added a 3rd pair of work photos to html

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const leftArrow = document.getElementById("leftArrow");
 
 const yardWork1 = document.getElementById("yardWork1");
 const yardWork2 = document.getElementById("yardWork2");
+const yardWork3 = document.getElementById("yardWork3");
 
 let yardWorkPairs = [
   { elem: yardWork1, display: "block" },
   { elem: yardWork2, display: "flex" },
+  { elem: yardWork3, display: "flex" },
 ];
 
 rightArrow.addEventListener("click", function () {
