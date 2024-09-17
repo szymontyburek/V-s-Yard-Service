a1850d2b5710c277f932893b602fe599d1c52dfe
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-17 11:27:15 -0700

    behavior of right arrow click is good

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ const leftArrow = document.getElementById("leftArrow");
 const yardWork1 = document.getElementById("yardWork1");
 const yardWork2 = document.getElementById("yardWork2");
 
-let yardWorkPairs = [yardWork1, yardWork2];
+let yardWorkPairs = [
+  { elem: yardWork1, display: "block" },
+  { elem: yardWork2, display: "flex" },
+];
 
 rightArrow.addEventListener("click", function () {
   let visibleIdx;
 
   for (let i = 0; i < yardWorkPairs.length; i++) {
-    const htmlElem = yardWorkPairs[i];
+    const htmlElem = yardWorkPairs[i].elem;
 
     if (htmlElem.style.display !== "none") {
+      if (i === yardWorkPairs.length - 1) visibleIdx = 0;
+      else visibleIdx = i + 1;
+
       htmlElem.style.display = "none";
-      visibleIdx = i + 1;
+
       break;
     }
   }
 
-  yardWorkPairs[visibleIdx].style.display = "flex";
+  const pair = yardWorkPairs[visibleIdx];
+  pair.elem.style.display = pair.display;
 });
