c4b4a7f0b0bd633dcc605b91e3a6d626e4c625a3
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-19 16:37:08 -0700

    added HTML for handyman section

--- script.js
+++ script.js
@@ -7,18 +7,23 @@ const yardWork3 = document.getElementById("yardWork3");
 
 let yardWorkPairs = [yardWork1, yardWork2, yardWork3];
 
+const handyman1 = document.getElementById("handyman1");
+const handyman2 = document.getElementById("handyman2");
+
+let handymanPairs = [handyman1, handyman2];
+
 const changePics = function () {
   let visibleIdx;
 
-  for (let i = 0; i < yardWorkPairs.length; i++) {
-    const htmlElem = yardWorkPairs[i];
+  for (let i = 0; i < handymanPairs.length; i++) {
+    const htmlElem = handymanPairs[i];
 
     if (htmlElem.style.display !== "none") {
       if (this.id === "rightArrow") {
-        if (i === yardWorkPairs.length - 1) visibleIdx = 0;
+        if (i === handymanPairs.length - 1) visibleIdx = 0;
         else visibleIdx = i + 1;
       } else {
-        if (i === 0) visibleIdx = yardWorkPairs.length - 1;
+        if (i === 0) visibleIdx = handymanPairs.length - 1;
         else visibleIdx = i - 1;
       }
 
@@ -28,7 +33,7 @@ const changePics = function () {
     }
   }
 
-  yardWorkPairs[visibleIdx].style.display = "flex";
+  handymanPairs[visibleIdx].style.display = "flex";
 };
 
 rightArrow.addEventListener("click", changePics.bind(rightArrow));
