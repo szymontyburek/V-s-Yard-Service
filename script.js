bd82de4ef4cba8050fc180ed0e36b222d36f85bb
Szymon Tyburek <szymontyburek10@gmail.com>
2024-09-19 15:57:09 -0700

    Home section (#16)
    * created a header with company name
    
    * created horizontal menu with hover effects
    
    * added images Ventura sent to an images folder
    
    * renamed yard work photos and added them to a designated folder
    
    * displaying a single pair of before and after pictures for yard work
    
    * adjusted styles of before and after headers for pictures
    
    * centered pictures horizontally
    
    * adjusted css to horizontally align pictures in parent div when it contains a border
    
    * adjusted certain styles and added html for arrows
    
    * added html for the next pair of photos then hid them (display: none) AND css for vertical photos
    
    * if photos are vertical, photos are displayed side by side
    
    * adjusted vertical alignment of div containing content and arrows
    
    * behavior of right arrow click is good
    
    * added a 3rd pair of work photos to html
    
    * left arrow functionality
    
    * mapped out variables in js for menu options (yard work, hauling, handyman) click functionality
    
    * functionality for Yard Work menu button
    
    * menu functionality
    
    * refactored menuContentObj conetents
    
    * adjusted css for contentVertical div(s) img elements
    
    * set media queries for .contentVertical img on small tables in landscape orientation
    
    * added media queries for small tablets in landscape orientation
    
    * fixed arrow click functionality bug
    
    * added media query to adjust styles for ALL tablets
    
    * stacking horizontal photos at all times
    
    * refactored js code
    
    * added space between stacked horizontal photos
    
    * refactored yardWorkPairs arr
    
    * incorporated picture of truck and trailer
    
    * cropped picture of truck only
    
    * added gray overlay to homeSection background photo
    
    * made a copy of homeSection div HTML. will use it for contact section later on
    
    * fixed HTML bug
    
    * added photos for home screen and chose 1 to display
    
    * company name AND slogan are over the home page photo
    
    * adjusted media queries for home page on landscape orientation devices
    
    * opacity does NOT affect home page text
    
    * applied media queries for font-size of home page text for tablet devices in ANY orientation
    
    * using different photo for landscape oriented devices

--- script.js
+++ script.js
@@ -1,1 +1,85 @@
-alert("hi");
+const rightArrow = document.getElementById("rightArrow");
+const leftArrow = document.getElementById("leftArrow");
+
+const yardWork1 = document.getElementById("yardWork1");
+const yardWork2 = document.getElementById("yardWork2");
+const yardWork3 = document.getElementById("yardWork3");
+
+let yardWorkPairs = [yardWork1, yardWork2, yardWork3];
+
+rightArrow.addEventListener("click", function () {
+  let visibleIdx;
+
+  for (let i = 0; i < yardWorkPairs.length; i++) {
+    const htmlElem = yardWorkPairs[i];
+
+    if (htmlElem.style.display !== "none") {
+      if (i === yardWorkPairs.length - 1) visibleIdx = 0;
+      else visibleIdx = i + 1;
+
+      htmlElem.style.display = "none";
+
+      break;
+    }
+  }
+
+  yardWorkPairs[visibleIdx].style.display = "flex";
+});
+
+leftArrow.addEventListener("click", function () {
+  let visibleIdx;
+
+  for (let i = 0; i < yardWorkPairs.length; i++) {
+    const htmlElem = yardWorkPairs[i];
+
+    if (htmlElem.style.display !== "none") {
+      if (i === 0) visibleIdx = yardWorkPairs.length - 1; //changed
+      else visibleIdx = i - 1; //changed
+
+      htmlElem.style.display = "none";
+
+      break;
+    }
+  }
+
+  yardWorkPairs[visibleIdx].style.display = "flex";
+});
+
+const menuContentObj = {
+  yardWorkBtn: {
+    btn: document.getElementById("yardWorkBtn"),
+    div: document.getElementById("yardWorkContent"),
+  },
+  haulingBtn: {
+    btn: document.getElementById("haulingBtn"),
+    div: document.getElementById("haulingContent"),
+  },
+  handymanBtn: {
+    btn: document.getElementById("handymanBtn"),
+    div: document.getElementById("handymanContent"),
+  },
+};
+
+const menuBtnIds = Object.keys(menuContentObj);
+
+for (const menuBtnId of menuBtnIds) {
+  menuContentObj[menuBtnId].btn.addEventListener("click", function () {
+    for (const menuBtnId of menuBtnIds) {
+      const menuBtn = menuContentObj[menuBtnId].btn;
+      const contentDiv = menuContentObj[menuBtnId].div;
+
+      if (contentDiv === menuContentObj[this.id].div) {
+        contentDiv.style.display = "flex";
+        menuBtn.style.backgroundColor = "black";
+        menuBtn.style.color = "white";
+      } else {
+        contentDiv.style.display = "none";
+        menuBtn.style.backgroundColor = "lightgray";
+        menuBtn.style.color = "black";
+      }
+    }
+  });
+}
+
+const yardWorkBtn = document.getElementById("yardWorkBtn");
+yardWorkBtn.click();
